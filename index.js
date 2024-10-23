093587800756605eae6a82399a009ab853b673f4
Emilia Junghanns <emijunghanns@hotmail.com>
2024-10-23 14:36:53 +0200

    done

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const s6 = `${s1} ${s2} ${s3} ${s4} ${s5}`
 
 // Print out the concatenated string
-
+//console.log(s6)
 
 
 
@@ -22,10 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const upperCasedI1 = part1.slice(0,3) + part1[3].toUpperCase()
+const upperCasedI2 = part2.slice(0,5) + part2[5].toUpperCase()
+const upperCasedString = upperCasedI1+upperCasedI2
 
 // Print the cameLtaiL-formatted string
-
+console.log(upperCasedI1,upperCasedI2)
 
 
 
@@ -34,11 +36,11 @@ const part2 = "script";
 *******************************************/
 const billTotal = 84;
 
-// Calculate the tip (15% of the bill total)
-
+// Calculate the tip (15% of the bill total
+const tip = billTotal*0.15
 
 // Print out the tipAmount
-
+console.log(tip)
 
 
 
@@ -47,10 +49,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const RandomNum = Math.ceil(Math.random()*9.99)
 
 // Print the generated random number
-
+console.log(RandomNum)
 
 
 /*******************************************
@@ -61,16 +63,17 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
-
-const expression2 = a || b;
-
-const expression3 = !a && b;
-
-const expression4 = !(a && b);
-
-const expression5 = !a || !b;
-
-const expression6 = !(a || b);
-
-const expression7 = a && a;
+const expression1 = a && b; // false
+console.log(expression1)
+const expression2 = a || b; //true
+console.log(expression2)
+const expression3 = !a && b; //false
+console.log(expression3)
+const expression4 = !(a && b); //true
+console.log(expression4)
+const expression5 = !a || !b; //false
+console.log(expression5)
+const expression6 = !(a || b); //flase
+console.log(expression6)
+const expression7 = a && a; //true
+console.log(expression7)
